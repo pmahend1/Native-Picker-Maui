98a6bdfd31661068df769b6240f35db5031750ad
Prateek Mahendrakar <prateek.mahendrakar@gmail.com>
2026-07-22 22:34:44 -0400

    Start of draft

--- src/MauiSwiftUiPicker/UI/NativePicker.cs
+++ src/MauiSwiftUiPicker/UI/NativePicker.cs
@@ -2,17 +2,21 @@ namespace MauiSwiftUiPicker.UI;
 
 public class NativePicker : View
 {
-    public static readonly BindableProperty ItemsSourceProperty =
-        BindableProperty.Create(nameof(ItemsSource), typeof(IList<string>), typeof(NativePicker), new List<string>());
-
-    public static readonly BindableProperty SelectedItemProperty =
-        BindableProperty.Create(nameof(SelectedItem), typeof(string), typeof(NativePicker), default(string), BindingMode.TwoWay);
-
+    public static readonly BindableProperty ItemsSourceProperty = BindableProperty.Create(propertyName: nameof(ItemsSource),
+                                                                                          returnType: typeof(IList<string>),
+                                                                                          declaringType: typeof(NativePicker),
+                                                                                          defaultValue: new List<string>());
     public IList<string> ItemsSource
     {
         get => (IList<string>)GetValue(ItemsSourceProperty);
         set => SetValue(ItemsSourceProperty, value);
     }
+
+    public static readonly BindableProperty SelectedItemProperty = BindableProperty.Create(propertyName: nameof(SelectedItem),
+                                                                                           returnType: typeof(string),
+                                                                                           declaringType: typeof(NativePicker),
+                                                                                           defaultValue: default(string),
+                                                                                           defaultBindingMode: BindingMode.TwoWay);
 
     public string SelectedItem
     {
